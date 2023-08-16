ac4fb479d99c44556e28d93208788ee84db58bba
yiits06 <yigit-karakoc@hotmail.com>
2023-08-17 02:38:57 +0300

    C# ile Tatil Köyü
    Tatil ücret hesaplama uygulaması

---
+++ Tatil Köyü/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Tatil_Köyü
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtYas = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtKonaklama = new System.Windows.Forms.ComboBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.lblTutar = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(58, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Yaş : ";
+            // 
+            // txtYas
+            // 
+            this.txtYas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtYas.Location = new System.Drawing.Point(167, 46);
+            this.txtYas.Margin = new System.Windows.Forms.Padding(2);
+            this.txtYas.Name = "txtYas";
+            this.txtYas.Size = new System.Drawing.Size(71, 26);
+            this.txtYas.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(40, 84);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Oda Tipi : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(5, 129);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Toplam Gün Sayısı :";
+            // 
+            // txtKonaklama
+            // 
+            this.txtKonaklama.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKonaklama.FormattingEnabled = true;
+            this.txtKonaklama.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50"});
+            this.txtKonaklama.Location = new System.Drawing.Point(154, 127);
+            this.txtKonaklama.Margin = new System.Windows.Forms.Padding(2);
+            this.txtKonaklama.Name = "txtKonaklama";
+            this.txtKonaklama.Size = new System.Drawing.Size(148, 28);
+            this.txtKonaklama.TabIndex = 4;
+            this.txtKonaklama.Text = "Konaklama Süresi";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnHesapla.FlatAppearance.MouseOverBackColor = System.Drawing.Color.IndianRed;
+            this.btnHesapla.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHesapla.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHesapla.ImageKey = "Custom-Icon-Design-Pretty-Office-11-Cash.ico";
+            this.btnHesapla.ImageList = this.ımageList1;
+            this.btnHesapla.Location = new System.Drawing.Point(347, 74);
+            this.btnHesapla.Margin = new System.Windows.Forms.Padding(2);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(160, 68);
+            this.btnHesapla.TabIndex = 6;
+            this.btnHesapla.Text = "Ücreti Hesapla ";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "Aha-Soft-Universal-Shop-Cash-register.ico");
+            this.ımageList1.Images.SetKeyName(1, "Custom-Icon-Design-Pretty-Office-11-Cash.ico");
+            this.ımageList1.Images.SetKeyName(2, "Oxygen-Icons.org-Oxygen-Actions-window-close (5).ico");
+            this.ımageList1.Images.SetKeyName(3, "Seanau-Email-Clear.ico");
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.ForeColor = System.Drawing.Color.Red;
+            this.lblDurum.Location = new System.Drawing.Point(197, 191);
+            this.lblDurum.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(21, 20);
+            this.lblDurum.TabIndex = 7;
+            this.lblDurum.Text = "...";
+            this.lblDurum.Click += new System.EventHandler(this.lblDurum_Click);
+            // 
+            // lblTutar
+            // 
+            this.lblTutar.AutoSize = true;
+            this.lblTutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTutar.ForeColor = System.Drawing.Color.Red;
+            this.lblTutar.Location = new System.Drawing.Point(197, 225);
+            this.lblTutar.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTutar.Name = "lblTutar";
+            this.lblTutar.Size = new System.Drawing.Size(21, 20);
+            this.lblTutar.TabIndex = 8;
+            this.lblTutar.Text = "...";
+            this.lblTutar.Click += new System.EventHandler(this.lblTutar_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.IndianRed;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.ImageKey = "Oxygen-Icons.org-Oxygen-Actions-window-close (5).ico";
+            this.button1.ImageList = this.ımageList1;
+            this.button1.Location = new System.Drawing.Point(538, 74);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 68);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Çıkış";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(40, 225);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Toplam Tutar (TL):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(58, 191);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "İndirim Durumu :";
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.IndianRed;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.ImageKey = "Seanau-Email-Clear.ico";
+            this.button2.ImageList = this.ımageList1;
+            this.button2.Location = new System.Drawing.Point(347, 169);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 68);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Alanları Temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Tek Kişilik",
+            "İki Kİşilik",
+            "Üç Kişilik",
+            "Dört Kişilik"});
+            this.comboBox1.Location = new System.Drawing.Point(154, 84);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(148, 28);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.Text = "Oda Tipi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(717, 275);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtKonaklama);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtYas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblTutar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Tatil Köyü";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtYas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox txtKonaklama;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.Label lblTutar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Tatil Köyü/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tatil_Köyü
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int günSayısı = Convert.ToInt32(txtKonaklama.Text);
+                int Yas = Convert.ToInt32(txtYas.Text);
+
+                if (Yas > 0 && Yas < 12)
+                {
+                    lblDurum.Text = "Ücretsiz";
+                    lblTutar.Text = "0";
+                }
+                if (Yas > 13 && Yas < 25)
+                {
+                    lblDurum.Text = "Genç";
+                    int Toplam = günSayısı * 50;
+                    lblTutar.Text = Toplam.ToString();
+
+                }
+                if (Yas >= 25 && Yas < 65)
+                {
+                    lblDurum.Text = "İndirim Yok";
+                    int Toplam = günSayısı * 100;
+                    lblTutar.Text = Toplam.ToString();
+                }
+                if (Yas >= 65)
+                {
+                    lblDurum.Text = "Yaslı İndirimi";
+                    int Toplam = günSayısı * 30;
+                    lblTutar.Text = Toplam.ToString();
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult cevap = MessageBox.Show("Çıkış Yyapmak istediginizden  eminmisiniz  ?","Çıkış",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if (cevap==DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtYas.Text = "";
+            txtKonaklama.Text = "";
+            comboBox1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDurum_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTutar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
